f9825a1c6c602989da77f7186ab79d591d8abbb7
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-07 18:45:37 -0500

    Mad Libs Game

---
+++ 107_building_MabLibs_Game/buildingMadLibsGame.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Roses are red" << endl;
+    cout << "Violets are blue" << endl;
+    cout << "I love you" << endl;
+
+
+    string color, pluralNoun, celebrity;
+
+    cout << "Enter a color: ";
+    getline(cin, color);
+
+    cout << "Enter a plural noun: ";
+    getline(cin, pluralNoun);
+
+    cout << "Enter a celebrity: ";
+    getline(cin, celebrity);
+
+    cout << "Roses are " << color << endl;
+    cout << pluralNoun << " are blue" << endl;
+    cout << "I love " << celebrity << endl;
+    return 0;
+}
