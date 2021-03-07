235a0df2963d223ebffb2c9df64c020e6def0d9c
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-06 22:12:12 -0500

    data_types.cpp

---
+++ 102_data_types/data_types.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    char grade = 'A';  // char use single quote 'A'
+    string phrase = "Lambda School"; // string use double quotes "Lambda School"
+
+    int age = 50; // int are whole numbers
+
+    // two types for decimals
+    // float and double : doubles are more precise in decimal places 
+    double gpa = 2.3;
+
+    // boolean
+    bool isActive = true;
+
+
+    cout << gpa << endl;
+
+    return 0;
+    
+}
