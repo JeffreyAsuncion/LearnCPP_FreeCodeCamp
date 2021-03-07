133f292adfaccc1686a639e43cc35e632b0bca9b
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-07 18:30:56 -0500

    basic calculator

---
+++ 106_buildingACalculator/buildingACalculator.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    // declare our num1, num2
+    int num1, num2;
+
+    cout << "Enter first number(integer): ";
+    cin >> num1;
+
+    cout << "Enter second number(integer): ";
+    cin >> num2;
+   
+    cout << num1 + num2 << endl;
+
+    double num3, num4;
+
+    cout << "Enter third number (decimal): ";
+    cin >> num3;
+
+    cout << "Enter fourth number (decimal): ";
+    cin >> num4;
+   
+    cout << num3 + num4 << endl;
+
+    return 0;
+}
