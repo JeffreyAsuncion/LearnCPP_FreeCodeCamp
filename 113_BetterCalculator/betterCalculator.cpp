f64e28360cb8ff36d7df243606bb24fb1bcc695c
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-13 10:32:38 -0500

    better calculator

--- 113_BetterCalculator/betterCalculator.cpp
+++ 113_BetterCalculator/betterCalculator.cpp
@@ -4,6 +4,33 @@ using namespace std;
 
 int main()
 {
+    // this calculator will have four functions
+    int num1, num2;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter operator: ";
+    cin >> op;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    int result;
+    if(op == '+'){
+        result = num1 + num2;
+    } else if(op == '-'){
+        result = num1 - num2;
+    } else if(op == '*'){
+        result = num1 * num2;
+    } else if(op == '/'){
+        result = num1 / num2;
+    } else {
+        cout << "\nInvalid Operator!\n";
+    }
+
+    cout << "\n";
+    cout << num1 << " " << op << " " << num2 << "  = ";
+    cout << result << endl;
 
     return 0;
 }
