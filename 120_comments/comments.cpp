d7113ec1ec7e138fa639cfc0cbd1d7839dcccbf0
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-13 22:19:18 -0500

    comments

---
+++ 120_comments/comments.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // this is a comment
+
+    /*
+    This is a multi line comment
+    asdf
+    asdf
+    as
+    dfa
+    sdf
+    asdf
+
+    */
+
+    // cout << "This is commented out" << endl;
+
+    return 0;
+}
